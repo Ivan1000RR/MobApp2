a3f231ebe6d641db719fbd279f0160e42f370b8d
Ivan1000RR <ividovic110@gmail.com>
2023-02-05 15:44:42 +0100

    Upload zadatka

---
+++ app/src/main/java/com/example/vidovic/mvvm/model/dao/MotorDAO.java
@@ -0,0 +1,29 @@
+package com.example.vidovic.mvvm.model.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.vidovic.mvvm.model.Motor;
+
+import java.util.List;
+
+
+@Dao
+public interface MotorDAO {
+
+    @Query("select * from motor order by proizvodjacMotora")
+    LiveData<List<Motor>> dohvatiMotore();
+
+    @Insert
+    void dodajNoviMotor(Motor motor);
+
+    @Update
+    void promjeniMotor(Motor motor);
+
+    @Delete
+    void obrisiMotor(Motor motor);
+}
---
+++ app/src/main/java/com/example/vidovic/mvvm/view/adapter/MotorClickListener.java
@@ -0,0 +1,7 @@
+package com.example.vidovic.mvvm.view.adapter;
+
+import com.example.vidovic.mvvm.model.Motor;
+
+public interface MotorClickListener {
+    void onItemClick(Motor motor);
+}
